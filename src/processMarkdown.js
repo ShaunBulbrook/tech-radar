9cd94b8acc9ad742b2ae414362174f1715b2d8b5
Shaun Bulbrook <shaun@shaunbulbrook.com>
2022-02-08 23:06:04 +0000

    Write out a tech radar ready json file from the markdown folder

---
+++ src/processMarkdown.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+const matter = require('gray-matter')
+const markdown = require('markdown-it')()
+
+const baseDirectory = './tech-radar-source/'
+const outputJSONFilename = 'tech-radar-source.json'
+
+const processMarkdown = () => {
+  fs.readdir(baseDirectory, {}, (error, files) => {
+    const convertedMarkdown = files.map((fileName) => matter(fs.readFileSync(baseDirectory + fileName)))
+    const techRadarReadyList = convertedMarkdown.reduce(
+      (accumulator, { data: { name, ring, quadrant, isNew }, content }) => {
+        return [
+          ...accumulator,
+          {
+            name,
+            ring,
+            quadrant,
+            isNew,
+            description: markdown.render(content),
+          },
+        ]
+      },
+      [],
+    )
+
+    fs.writeFile(`${baseDirectory}${outputJSONFilename}`, JSON.stringify(techRadarReadyList), () => {})
+  })
+}
+console.log(processMarkdown())
